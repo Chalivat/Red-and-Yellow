c48bc96505d33f76a483769e09393e29ec632fd7
LeoDiringer <52820052+LeoDiringer@users.noreply.github.com>
2019-12-02 16:45:49 +0100

    g

---
+++ Red and Yellow/Assets/Dialogue_List.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialogue_List : MonoBehaviour
+{
+    public Transform playerTransform;
+    public string[] dialogue;
+    public Text playerText;
+    
+    int count = -2;
+
+    private void OnEnable()
+    {
+        count += 1;
+    }
+
+    void Update()
+    {
+        transform.position = playerTransform.position;
+        Display();
+    }
+
+    void Display()
+    {
+        playerText.text = dialogue[count];
+    }
+}

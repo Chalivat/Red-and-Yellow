a9c6488a8cbaff09398a0fb0b2206789712a3b0d
LeoDiringer <52820052+LeoDiringer@users.noreply.github.com>
2019-12-12 13:14:25 +0100

    Side View fixed

--- Red and Yellow/Assets/ChangeMode.cs
+++ Red and Yellow/Assets/ChangeMode.cs
@@ -5,13 +5,81 @@ using UnityEngine;
 public class ChangeMode : MonoBehaviour
 {
     public GameObject cible;
+    public GameObject cam;
+    public GameObject vCam;
+    public bool isCam = false;
+    public Vector3 inheritRotation;
+
+    GameObject player;
+    bool isTriggered = false;
+    int count = 0;
+
+    private void Update()
+    {
+        Debug.Log(count);
+
+        if (isTriggered)
+        {
+            count += 1;
+
+            if(count == 1)
+            {
+                player.GetComponent<Player_Movement>().isTopView = !player.GetComponent<Player_Movement>().isTopView;
+                player.GetComponent<Player_Movement>().enabled = false;
+                player.GetComponent<Player_Shoot>().enabled = false;
+            }
+            if(count == 2)
+            {
+                player.transform.position = cible.transform.position;
+                player.transform.rotation = Quaternion.LookRotation(inheritRotation);
+                isCam = !isCam;
+
+                if (player.GetComponent<Player_Movement>().isTopView)
+                {
+                    player.GetComponent<Player_Movement>().cam = vCam;
+                    player.GetComponent<Player_Shoot>().cam = vCam;
+                }
+                else
+                {
+                    player.GetComponent<Player_Movement>().cam = cam;
+                    player.GetComponent<Player_Shoot>().cam = cam;
+                }
+                CameraSpawner();
+            }
+            if(count == 3)
+            {
+                player.GetComponent<Player_Movement>().enabled = true;
+                player.GetComponent<Player_Shoot>().enabled = true;
+                player.GetComponent<Jump>().enabled = true;
+            }
+            if(count == 4)
+            {
+                isTriggered = false;
+                count = 0;
+            }
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            other.GetComponent<Player_Movement>().isTopView = !other.GetComponent<Player_Movement>().isTopView;
-            other.transform.position = cible.transform.position;
+            player = other.gameObject;
+            isTriggered = true;
+        }
+    }
+
+    void CameraSpawner()
+    {
+        if (isCam)
+        {
+            cam.SetActive(true);
+            vCam.SetActive(false);
+        }
+        else
+        {
+            cam.SetActive(false);
+            vCam.SetActive(true);
         }
     }
 }

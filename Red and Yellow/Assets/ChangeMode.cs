f87e46935fdd2318d801496fe102a039ea6580a8
LeoDiringer <52820052+LeoDiringer@users.noreply.github.com>
2019-12-12 14:06:35 +0100

    ChangeMode fixed

--- Red and Yellow/Assets/ChangeMode.cs
+++ Red and Yellow/Assets/ChangeMode.cs
@@ -11,17 +11,17 @@ public class ChangeMode : MonoBehaviour
     public Vector3 inheritRotation;
 
     GameObject player;
+    GameObject player2;
     bool isTriggered = false;
     int count = 0;
+    int playerCount = 0;
 
     private void Start()
     {
-        cam.SetActive(false);
+        CameraSpawner();
     }
     private void Update()
     {
-        Debug.Log(count);
-
         if (isTriggered)
         {
             count += 1;
@@ -31,22 +31,31 @@ public class ChangeMode : MonoBehaviour
                 player.GetComponent<Player_Movement>().isTopView = !player.GetComponent<Player_Movement>().isTopView;
                 player.GetComponent<Player_Movement>().enabled = false;
                 player.GetComponent<Player_Shoot>().enabled = false;
+                player2.GetComponent<Player_Movement>().isTopView = !player.GetComponent<Player_Movement>().isTopView;
+                player2.GetComponent<Player_Movement>().enabled = false;
+                player2.GetComponent<Player_Shoot>().enabled = false;
             }
             if(count == 2)
             {
                 player.transform.position = cible.transform.position;
                 player.transform.rotation = Quaternion.LookRotation(inheritRotation);
+                player2.transform.position = cible.transform.position;
+                player2.transform.rotation = Quaternion.LookRotation(inheritRotation);
                 isCam = !isCam;
 
                 if (player.GetComponent<Player_Movement>().isTopView)
                 {
                     player.GetComponent<Player_Movement>().cam = vCam;
                     player.GetComponent<Player_Shoot>().cam = vCam;
+                    player2.GetComponent<Player_Movement>().cam = vCam;
+                    player2.GetComponent<Player_Shoot>().cam = vCam;
                 }
                 else
                 {
                     player.GetComponent<Player_Movement>().cam = cam;
                     player.GetComponent<Player_Shoot>().cam = cam;
+                    player2.GetComponent<Player_Movement>().cam = cam;
+                    player2.GetComponent<Player_Shoot>().cam = cam;
                 }
                 CameraSpawner();
             }
@@ -55,6 +64,9 @@ public class ChangeMode : MonoBehaviour
                 player.GetComponent<Player_Movement>().enabled = true;
                 player.GetComponent<Player_Shoot>().enabled = true;
                 player.GetComponent<Jump>().enabled = true;
+                player2.GetComponent<Player_Movement>().enabled = true;
+                player2.GetComponent<Player_Shoot>().enabled = true;
+                player2.GetComponent<Jump>().enabled = true;
             }
             if(count == 4)
             {
@@ -68,13 +80,23 @@ public class ChangeMode : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            player = other.gameObject;
-            isTriggered = true;
+            playerCount += 1;
+            if(playerCount == 1)
+            {
+                player = other.gameObject;
+            }
+            if(playerCount >= 2)
+            {
+                player2 = other.gameObject;
+                isTriggered = true;
+                playerCount = 0;
+            }
         }
     }
 
     void CameraSpawner()
     {
+        Debug.Log("COUCPU");
         if (isCam)
         {
             cam.SetActive(true);

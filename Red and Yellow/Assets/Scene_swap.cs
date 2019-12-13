7f5d6f9351abdaa87a82415fbf4c27fc92d74420
LeoDiringer <52820052+LeoDiringer@users.noreply.github.com>
2019-12-13 17:51:39 +0100

    Bla bla

---
+++ Red and Yellow/Assets/Scene_swap.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_swap : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.transform.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("Boss Scene 1");
+        }
+    }
+}
